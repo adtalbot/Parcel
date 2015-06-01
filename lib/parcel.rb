9ee6af74e96ecc4a9d7963d4b4e3ce08f43536d7
audrie & debbie <student@epicodus.com>
2015-06-01 11:57:36 -0700

    Passes the distance calculation spec.

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -2,11 +2,12 @@ class Parcel
 
 @@all_parcels = []
 
-  define_method(:initialize) do |height, width, length, weight|
+  define_method(:initialize) do |height, width, length, weight, distance|
     @weight = weight
     @height = height
     @width = width
     @length = length
+    @distance = distance
   end
 
   define_method(:weight) do
@@ -34,10 +35,23 @@ class Parcel
   end
 
   define_method(:volume) do
-    @volume = (@width *@height *@length)
+    @volume = (@width * @height * @length)
   end
 
   define_method(:speed) do
     speed = {'expedited' => 3, 'two_day' => 2, 'ground' => 1}
+  end
+
+  define_method(:distance_calculation) do
+    if @distance > 0 && (@distance < 100)
+      @distance_value = 1
+    elsif @distance > 100 && (@distance < 500)
+      @distance_value = 2
+    elsif @distance > 500 && (@distance < 1000)
+      @distance_value = 3
+    else
+      'we do not ship that far'
+    end
+    @distance_value
   end
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -1,5 +1,6 @@
 require('rspec')
 require('parcel')
+require('pry')
 
 describe(Parcel) do
   before() do
@@ -12,28 +13,34 @@ describe(Parcel) do
   end
   describe('#save') do
     it('adds a property of the parcel and saves it') do
-      test_parcel = Parcel.new(3,6,2,10)
+      test_parcel = Parcel.new(3,6,2,10,100)
       test_parcel.save()
       expect(Parcel.all()).to(eq([test_parcel]))
     end
   end
   describe('.clear') do
     it('empties out all of the saved parcels') do
-      Parcel.new(3,6,2,10).save()
+      Parcel.new(3,6,2,10,100).save()
       Parcel.clear()
       expect(Parcel.all()).to(eq([]))
     end
   end
     describe('#volume') do
       it('calculates the volume of a parcel') do
-        test_parcel = Parcel.new(3,6,2,10)
+        test_parcel = Parcel.new(3,6,2,10,100)
         expect(test_parcel.volume()).to(eq(36))
       end
     end
     describe('#speed') do
       it('determines how quickly a package will ship') do
-        test_parcel = Parcel.new(3,6,2,1)
+        test_parcel = Parcel.new(3,6,2,1,100)
         expect(test_parcel.speed().fetch('expedited')).to(eq(3))
+    end
+  end
+    describe('#distance_calculation') do
+      it('calculates cost of shipping by distance') do
+        test_parcel = Parcel.new(3,6,2,1,99)
+        expect(test_parcel.distance_calculation()).to(eq(1))
     end
   end
 end
