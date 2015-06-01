03c70739732f54424e9650eb8692ca88afa30dfe
audrie & debbie <student@epicodus.com>
2015-06-01 10:22:30 -0700

    Passes the volume spec.

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -2,10 +2,11 @@ class Parcel
 
 @@all_parcels = []
 
-  define_method(:initialize) do |weight, height, width|
+  define_method(:initialize) do |height, width, length, weight|
     @weight = weight
     @height = height
     @width = width
+    @length = length
   end
 
   define_method(:weight) do
@@ -16,7 +17,7 @@ class Parcel
     @height
   end
 
-  define_method(:weight) do
+  define_method(:width) do
     @width
   end
 
@@ -30,5 +31,9 @@ class Parcel
 
   define_singleton_method(:clear) do
     @@all_parcels = []
+  end
+
+  define_method(:volume) do
+    @volume = (@width *@height *@length)
   end
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -1,8 +1,10 @@
 require('rspec')
 require('parcel')
 
-
 describe(Parcel) do
+  before() do
+    Parcel.clear()
+  end
   describe('.all') do
     it('is empty at first') do
       expect(Parcel.all()).to(eq([]))
@@ -10,16 +12,22 @@ describe(Parcel) do
   end
   describe('#save') do
     it('adds a property of the parcel and saves it') do
-      test_parcel = Parcel.new(3,6,2)
+      test_parcel = Parcel.new(3,6,2,10)
       test_parcel.save()
       expect(Parcel.all()).to(eq([test_parcel]))
     end
   end
   describe('.clear') do
     it('empties out all of the saved parcels') do
-      Parcel.new(3,6,2).save()
+      Parcel.new(3,6,2,10).save()
       Parcel.clear()
       expect(Parcel.all()).to(eq([]))
     end
   end
-end
+    describe('#volume') do
+      it('calculates the volume of a parcel') do
+        test_parcel = Parcel.new(3,6,2,10)
+        expect(test_parcel.volume()).to(eq(36))
+      end
+    end
+ end
